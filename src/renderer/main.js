10e4bc2200ed504bc782eddcf21f82c8db7a0a31
崔琼雪 <cuiqiongxue@doumi.com>
2018-01-25 14:12:05 +0800

    add

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,7 +4,12 @@ import App from './App'
 import router from './router'
 import store from './store'
 import iView from 'iview'
-if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+const ve = {
+    install: function (Vue) {
+        Vue.prototype.$electron = require('electron')
+    }
+}
+if (!process.env.IS_WEB) Vue.use(ve);
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 Vue.use(iView)
