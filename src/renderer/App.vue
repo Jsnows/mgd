1caf162ba5652e6e8e96bf52a6d498ff09cf67ad
崔琼雪 <cuiqiongxue@doumi.com>
2018-03-29 10:53:46 +0800

    fix:打印链接

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -190,7 +190,6 @@
                 setTimeout(function(){
                     const shell = require('electron').shell
                     const links = document.querySelectorAll('a[href]')
-                    console.log(links);
                     Array.prototype.forEach.call(links, function (link) {
                       const url = link.getAttribute('href')
                       if (url.indexOf('http') === 0) {
